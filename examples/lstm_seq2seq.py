893a25f60b769ddf19034ba61031fb25b42681bc
maKunugi <masan580@gmail.com>
2020-09-05 17:37:07 +0900

    データを変更

--- examples/lstm_seq2seq.py
+++ examples/lstm_seq2seq.py
@@ -95,7 +95,8 @@ for line in lines[: min(num_samples, len(lines) - 1)]:
                 print(mrph.midasi)
                 input_characters.add(mrph.midasi)
         for mrph in wakati_target.mrph_list():
-            target_wakati_words.append(mrph.midasi)
+            if mrph != '\t':
+                target_wakati_words.append(mrph.midasi)
             if mrph.midasi not in target_characters:
                 print(mrph.midasi)
                 target_characters.add(mrph.midasi)
